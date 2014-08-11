bb4ef64741d500aa9357b98581ecb8b090894c63
SIDDHARTH BHOLA <siddharthbhola123@yahoo.co.in>
2014-08-11 09:54:11 +0530

    Enumerated Directories. Getting folder names from a directory done.

--- TMDB/TMDB/Program.cs
+++ TMDB/TMDB/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace TMDB
 {
@@ -10,9 +11,34 @@ namespace TMDB
     {
         static void Main(string[] args)
         {
-            string movieName = "The lord of the rings";
-            TMDBAPI.TmdbRequest(movieName);
+            ///retrieve all the directorty folder names 
+            string dirPath = @"D:\";
 
+            try
+            {
+                List<string> dirs = new List<string>(Directory.EnumerateDirectories(dirPath));
+
+                foreach (var dir in dirs)
+                {
+                    Console.WriteLine(dir.Substring(dir.LastIndexOf("\\")+1));
+                }
+                Console.WriteLine("{0} Directories found",dirs.Count);
+            }
+            catch (UnauthorizedAccessException UAEx)
+        {
+            Console.WriteLine(UAEx.Message);
+        }
+        catch (PathTooLongException PathEx)
+        {
+            Console.WriteLine(PathEx.Message);
+        }
+                
+
+
+
+            ///Give a call to TMDB API
+            //string movieName = "The lord of the rings";
+            //TMDBAPI.TmdbRequest(movieName);
         }
     }
 }
